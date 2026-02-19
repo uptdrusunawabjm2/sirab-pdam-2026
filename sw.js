83eb550f809fcf1fd447ccd8f5bd3d3e02617518
uptdrusunawabjm2 <uptd.rusunawa.bjm2@gmail.com>
2026-02-19 23:53:16 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,6 +12,10 @@ self.addEventListener("install", e=>{
   self.skipWaiting();
 });
 
+self.addEventListener("activate", e=>{
+  e.waitUntil(self.clients.claim());
+});
+
 self.addEventListener("fetch", e=>{
   e.respondWith(
     caches.match(e.request).then(r=> r || fetch(e.request))
