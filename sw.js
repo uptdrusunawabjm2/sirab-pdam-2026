3bf3bb9c6507691443dced398cbaa61190c01626
uptdrusunawabjm2 <uptd.rusunawa.bjm2@gmail.com>
2026-02-19 23:47:20 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = "pdam-cache-v1";
+
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll([
+      "/petugas",
+      "/manifest.json"
+    ]))
+  );
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request))
+  );
+});
